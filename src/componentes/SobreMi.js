82b283ac51ce695192c9ce13576202207f1aa583
Andrea Carolina <anndiegonzalez@gmail.com>
2020-10-21 10:58:13 -0500

    actualizacion

--- src/componentes/SobreMi.js
+++ src/componentes/SobreMi.js
@@ -13,7 +13,7 @@ const SobreMi = () => {
                     Oriunda de Barranquilla y paisa de corazón, siempre he estado interesada por el arte, la música y el diseño, llegué a Medellín hace unos diez años y me radiqué con mi familia.
                     </p>
                     <p>
-                    Soy una mujer que no se cansa de aprender, terminé mis estudios de Producción de Televisión en el Poli JIC, luego me dediqué por mucho tiempo a trabajar en el área comercial, se me presentó la oportunidad de estudiar Desarrollo de Software con la alcaldía de Medellín y ahora estoy realizando un Diplomado con Academía Geek de React y Node.
+                    Soy una mujer que no se cansa de aprender, terminé mis estudios de Producción de Televisión en el Poli JIC, luego me dediqué por mucho tiempo a trabajar en el área comercial, hoy en día soy Técnica en Desarrollo de Software me gradue en el Cesde, y tengo un Diplomado con Academía Geek de React y NodeJS.
                     </p>
                     <p>
                     Me apasiona el Front-end, siento que puedo expresarme y poner un granito de arena de lo que poco o mucho que sé en los diferentes proyectos. Me gustaría motivar a muchas mas mujeres que quieran entrar en el mundo de la programación.
